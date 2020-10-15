f62ed91abff6415286cae18400f84cf7d455e36f
Rashi Kulshrestha <rashikul5.rk@gmail.com>
2020-10-15 18:54:59 +0530

    Add csv file in Dashboard.js

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,13 +1,27 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, useState } from "react";
+import * as d3 from 'd3';
+import data from "./data.csv";
+import Table from "../table/Table";
 
-class Dashboard extends Component{
-    render(){
-        return(
-            <Fragment>
-                <div>Home</div>
-            </Fragment>
-        )
-    }
+const Dashboard = () => {
+    const [content, setContent] = useState([]);
+    useEffect(() => {
+        //reference taken by stackoverflow
+        d3.csv(data).then(function(content) {
+            setContent(content);
+        }).catch(function(err) {
+            throw err;
+        })
+    },[]);
+    var table = content;
+    console.log(table);
+    return(
+        <div>
+            <header>List of Orders</header>
+            <main>
+                <Table data ={table}/>
+            </main>
+        </div>
+    )
 }
-
 export default Dashboard;
